0b5d609cff315dbefb578d31d638fe6a7f1596d0
Evelyn Pan <evelyn.yinghan.pan@gmail.com>
2013-07-02 15:41:25 -0700

    initial commit

---
+++ 37signals.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+	$('#basecamp').mouseover(function() { 
+	console.log("mouseoverworks!");
+});
+ 
+  
+});
+
+/*
+two things:
+1. Make HTML for the arrows
+2.make CSS for the arrows using z-index whichs put layer, (put position absolte or position relative)
+3. Hide/Show layers in Jquery
+
+display:none for the arrow
+
+*/
+
+
+
